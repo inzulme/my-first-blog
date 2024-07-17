39d8e550e0adde9082890c3c114649cc5ee5749f
test <hujj4736@gmail.com>
2024-07-17 13:25:18 +0900

    add function login and logout.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,9 @@ urlpatterns = [
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
   path('post/new', views.post_new, name='post_new'),	
   path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+  path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+  path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+  path('draft/', views.post_draft_list, name='post_draft_list'),
+  path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+  path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from .forms import PostForm
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 
 # Create your views here.
@@ -15,6 +17,7 @@ def post_detail(request,pk):
   post = get_object_or_404(Post, pk=pk)
   return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
   if request.method == "POST":
     form = PostForm(request.POST)
@@ -22,13 +25,13 @@ def post_new(request):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
   else:
     form = PostForm()
   return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request,pk):
   post = get_object_or_404(Post, pk=pk)
   if request.method == "POST":
@@ -36,9 +39,32 @@ def post_edit(request,pk):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
   else:
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+  posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+  return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.publish()
+  return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.delete()
+  return redirect('post_list')
+
+def login(request):
+  return render(request, 'registration/login.html', {})
+
+def logout(request):
+  django_logout(request)
+  return redirect('post_list')
